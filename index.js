b022607eadc411fa23f3acab7e8f61381bdf10b2
Dallas <dallas.oliver91@gmail.com>
2019-09-08 18:46:43 -0400

    using custom timer class for timer functionality

--- index.js
+++ index.js
@@ -1,80 +1,109 @@
-const startButton = document.getElementById("test");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
 const resetButton = document.getElementById("reset");
 const container = document.getElementById("container");
 const totalTimerDisplay = document.getElementById("total-timer");
+const timer = new Timer();
+let timerStarted;
 
-let seconds = 0;
-let milliseconds = 0;
-let s = "00";
-let ms = "00";
-let timer;
-let buttonSwitch = true;
-
-function playGame() {
-  startButton.textContent = "Stop";
+function startButtonPressed(e) {
+  e.preventDefault();
   let randomTime = Math.floor(Math.random() * 10 * 1000) + 900;
-  setTimeout(() => changeBackground(), randomTime);
-
-  startButton.removeEventListener("click", () => playGame());
-  startButton.addEventListener("click", () => stopTimer());
+  setTimeout(startTimer, randomTime);
 }
 
-function changeBackground() {
-  startTimer();
-  startButton.textContent = "Stop";
-  container.style.backgroundColor = "red";
-}
-
-function countTimer() {
-  milliseconds++;
-
-  if (milliseconds >= 100) {
-    milliseconds = 0;
-    seconds++;
-  }
-
-  s = seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00";
-  ms = milliseconds > 9 ? milliseconds : "0" + milliseconds;
-
-  totalTimerDisplay.textContent = s + ":" + ms + " " + "ms";
-
-  timerDuration();
-}
-
-function timerDuration() {
-  if (seconds !== 99) {
-    timer = setTimeout(countTimer, 10);
-  }
-}
-
-function stopTimer() {
-  clearTimeout(timer);
+function stopButtonPressed() {
+  timerStarted = false;
+  timer.stop();
+  changeStyles("darkcyan");
+  displayResults(timer.elapsed());
+  return timer.elapsed();
 }
 
 function startTimer() {
-  buttonSwitch = !buttonSwitch;
-
-  if (buttonSwitch == false) {
-    timerDuration();
-  } else {
-    stopTimer();
-  }
+  timerStarted = true;
+  timer.start();
+  changeStyles("red");
 }
 
-function resetTimer() {
-  buttonSwitch = true;
-  startButton.textContent = "Start";
-  container.style.backgroundColor = "darkcyan";
-  clearTimeout(timer);
-
-  minutes = 0;
-  seconds = 0;
-  milliseconds = 0;
-  m = "";
-  s = "";
-  ms = "";
-  totalTimerDisplay.textContent = "00" + ":" + "00" + " " + "ms";
+function changeStyles(color) {
+  container.style.backgroundColor = color;
 }
 
-startButton.addEventListener("click", () => playGame());
+function displayResults(time) {
+  totalTimerDisplay.textContent = timeInCorrectFormat(time);
+}
+
+function timeInCorrectFormat(time) {
+  let seconds = Math.floor(Math.abs(time));
+  let milliSeconds = Math.floor((Math.abs(time) * 60) % 60);
+  console.log(seconds, milliSeconds);
+  return (
+    (seconds < 10 ? "0" : "") +
+    seconds +
+    ":" +
+    (milliSeconds < 10 ? "0" : "") +
+    milliSeconds +
+    " " +
+    "ms"
+  );
+}
+
+startButton.addEventListener("click", e => startButtonPressed(e));
+stopButton.addEventListener("click", e => stopButtonPressed(e));
 resetButton.addEventListener("click", () => resetTimer());
+
+// let seconds = 0;
+// let milliseconds = 0;
+// let s = "00";
+// let ms = "00";
+// let timer;
+// let timerIsRunning = false;
+
+// function timerSwitch() {
+//   timerIsRunning = !timerIsRunning;
+
+//   if (!timerIsRunning) {
+//     stopTimer();
+//   } else {
+//     startTimer();
+//   }
+// }
+
+// function startTimer() {
+//   timer = setTimeout(countTimer, 10);
+// }
+
+// function countTimer() {
+//   milliseconds++;
+
+//   if (milliseconds >= 100) {
+//     milliseconds = 0;
+//     seconds++;
+//   }
+
+//   s = seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00";
+//   ms = milliseconds > 9 ? milliseconds : "0" + milliseconds;
+
+//   totalTimerDisplay.textContent = s + ":" + ms + " " + "ms";
+
+//   startTimer();
+// }
+
+// function stopTimer() {
+//   clearTimeout(timer);
+// }
+
+// function resetTimer() {
+//   timerIsRunning = true;
+//   changeStyles("Start", "darkcyan");
+
+//   clearTimeout(timer);
+
+//   seconds = 0;
+//   milliseconds = 0;
+
+//   s = "";
+//   ms = "";
+//   totalTimerDisplay.textContent = "00" + ":" + "00" + " " + "ms";
+// }
---
+++ timer.js
@@ -0,0 +1,45 @@
+class Timer {
+  constructor() {
+    this.startTime = null;
+    this.stopTime = null;
+    this.running = false;
+    this.seconds = 0;
+    this.milliseconds = 0;
+    this.s = "00";
+    this.ms = "00";
+  }
+
+  getTime() {
+    const day = new Date();
+    return day.getTime();
+  }
+
+  start() {
+    if (this.running == true) {
+      return;
+    } else if (this.startTime !== null) {
+      this.stopTime = null;
+    }
+    this.running = true;
+    this.startTime = this.getTime();
+    return this.startTime;
+  }
+
+  stop() {
+    if (this.running == false) {
+      return;
+    }
+
+    this.stopTime = this.getTime();
+    this.running = false;
+    return this.stopTime;
+  }
+
+  elapsed() {
+    if (this.startTime == null || this.stopTime == null) {
+      return "undefined";
+    } else {
+      return (this.stopTime - this.startTime) / 1000;
+    }
+  }
+}
